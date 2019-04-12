5de760b221ee26fdfe88b59501561200ac947b76
Mohammed <moha.khamis@outlook.com>
2019-04-12 19:56:14 +0200

    Fixed responsivity and changed colors

--- controllers/search.js
+++ controllers/search.js
@@ -111,7 +111,7 @@ const addDirectionsTo = (array, source, destination) => {
 
       newPath.push({
         direction,
-        stops: [first]
+        stops: [first, second]
       });
     }
   }
