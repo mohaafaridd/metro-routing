ad48b70fd50a4cd6ab5c84ad58d7cb416a8daa30
Mohammed <moha.khamis@outlook.com>
2019-10-31 23:47:50 +0200

    created line 3

---
+++ server/lines/lineThree.js
@@ -0,0 +1,159 @@
+const { stop } = require('./helper');
+const line = [
+  stop(
+    {
+      english: 'Imbaba',
+      arabic: 'إمبابة',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'El-Sudan',
+      arabic: 'السودان',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'El-Zamalek',
+      arabic: 'الزمالك',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Maspero',
+      arabic: 'ماسبيرو',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Nasr',
+      arabic: 'ناصر',
+    },
+    [1, 3]
+  ),
+  stop(
+    {
+      english: 'El-Attba',
+      arabic: 'العتبة',
+    },
+    [2, 3]
+  ),
+  stop(
+    {
+      english: 'Bab El-Sheereya',
+      arabic: 'باب الشعرية',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'El-Geish',
+      arabic: 'الجيش',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Abdo Basha',
+      arabic: 'عبده باشا',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'El-Abbasya',
+      arabic: 'العباسية',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'El-Maarad',
+      arabic: 'المعرض',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Cairo Stadium',
+      arabic: 'إستاد القاهرة',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Girl College',
+      arabic: 'كلية البنات',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Pyramids',
+      arabic: 'الاهرام',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Haroun',
+      arabic: 'هارون',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Heliopolis ',
+      arabic: 'هليوبوليس',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Alf Maskan',
+      arabic: 'الف مسكن',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Arab',
+      arabic: 'العرب',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Ain Shams 1',
+      arabic: 'عين شمس 1',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Ain Shams 2',
+      arabic: 'عين شمس 2',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Omar Inb El-Khattab',
+      arabic: 'عمر ابن الخطاب',
+    },
+    [3]
+  ),
+  stop(
+    {
+      english: 'Cairo Airport',
+      arabic: 'مطار القاهرة',
+    },
+    [3]
+  ),
+];
+
+module.exports = line;
--- server/metroGraph.js
+++ server/metroGraph.js
@@ -3,6 +3,7 @@ const graph = Graph();
 
 const lineOne = require('./lines/linesOne');
 const lineTwo = require('./lines/lineTwo');
+const lineThree = require('./lines/lineThree');
 
 // Line 1
 lineOne.forEach((stop, i, line) => {
@@ -18,6 +19,13 @@ lineTwo.forEach((stop, i, line) => {
   }
 });
 
-console.log(graph.nodes());
+// Line 3
+lineThree.forEach((stop, i, line) => {
+  if (i < lineThree.length - 1) {
+    graph.addEdge(line[i].name.english, line[i + 1].name.english);
+  }
+});
+
+console.log(graph.nodes().length);
 
 return graph;
