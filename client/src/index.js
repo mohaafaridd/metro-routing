cd4d3465d560c430a6f580d9c11b1e0a4850bbc5
Mohammed <moha.khamis@outlook.com>
2019-11-01 20:42:13 +0200

    tailwind setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
+import './styles/tailwind.css';
 ReactDOM.render(<App />, document.getElementById('root'));
