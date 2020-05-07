fc261deb7adda9e797ac860fa31d16ed216857b1
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-05-07 17:27:41 +0200

    formated station component

--- src/components/Station.tsx
+++ src/components/Station.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Station as IStation } from '../interfaces/station'
+import { FaExchangeAlt } from 'react-icons/fa'
 import PathContext from '../context/path/pathContext'
 import LanguageContext from '../context/language/languageContext'
 
@@ -14,7 +15,12 @@ const Station = ({ station, index }: { station: IStation; index: number }) => {
     path[index + 1]?.line !== path[index - 1]?.line
 
   return (
-    <li className='station'>
+    <li className='station' dir={language === 'ARABIC' ? 'rtl' : 'ltr'}>
+      {changingStation && (
+        <div className='changing-icon'>
+          <FaExchangeAlt />
+        </div>
+      )}
       <p>
         {language === 'ARABIC' ? station.name.arabic : station.name.english}
       </p>
