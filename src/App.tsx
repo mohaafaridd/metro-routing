524063dfd293bc20b3c6312e9af27be8e85a0dd2
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-05-07 16:54:15 +0200

    added custom backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Search from './components/Search'
 import Path from './components/Path'
+import ThemeContext from './context/theme/themeContext'
 const App = () => {
+  const { theme } = useContext(ThemeContext)
+
   return (
-    <div>
-      <h1>Egyptian Metro Lines</h1>
+    <div id='app' className={theme}>
+      <h1 className='text-gray-500'>Egyptian Metro Lines</h1>
       <Search />
       <Path />
     </div>
