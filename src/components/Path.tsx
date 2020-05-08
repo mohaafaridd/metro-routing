b8d238a5663a1c9c4a03b62b53b2aa3f5a30a1e7
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-05-08 07:49:38 +0200

    created utility to get proper direction

--- src/components/Path.tsx
+++ src/components/Path.tsx
@@ -1,11 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import PathContext from '../context/path/pathContext'
 import Station from './Station'
 import LanguageContext from '../context/language/languageContext'
+import { getDirections } from '../utils/getDirections'
 
 const Path = () => {
   const { language } = useContext(LanguageContext)
   const { path } = useContext(PathContext)
+  const [directions, setDirections] = useState(getDirections(path))
+
+  useEffect(() => {
+    setDirections(getDirections(path))
+  }, [path])
 
   return (
     <div>
---
+++ src/utils/getDirections.ts
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+import { Station } from '../interfaces/station'
+import { lineOne, lineTwo, lineThree } from '../database/lines'
+
+export const getDirections = (path: Station[]) => {
+  const lines = [lineOne, lineTwo, lineThree]
+
+  const directions = _.uniq(
+    path.map((station, index) => {
+      if (index === path.length - 1) return
+
+      // Destination
+      const destination = path[index + 1]
+
+      // Getting the wanted line index
+      const lineIndex = _.intersection(station.line, destination.line)[0]
+      const line = lines[lineIndex - 1]
+
+      // Getting proper direction trigger (-) from top to bottom (+) from bottom to top
+      const trigger =
+        line.stations.findIndex(
+          (source) => source.name.english === station.name.english
+        ) -
+        line.stations.findIndex(
+          (source) => source.name.english === station.name.english
+        )
+
+      if (trigger < 0) return JSON.stringify(line.direction.start)
+      else return JSON.stringify(line.direction.end)
+    })
+  )
+
+  return directions
+}
