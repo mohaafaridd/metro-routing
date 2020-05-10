823cc6874ab9f8ba8a1954c3c7aeb299b4ab7fe0
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-05-10 21:33:54 +0200

    added localstorage support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ThemeContext, LanguageContext } from './context'
 import Search from './components/Search'
 import Path from './components/Path'
-import ThemeContext from './context/theme/themeContext'
 import ThemeToggler from './components/ThemeToggler'
 import LanguageToggler from './components/LanguageToggler'
+import { Theme } from './interfaces/context/theme'
+import { Language } from './interfaces/context/language'
 const App = () => {
-  const { theme } = useContext(ThemeContext)
+  const { theme, setTheme } = useContext(ThemeContext)
+  const { language, setLanguage } = useContext(LanguageContext)
+
+  useEffect(() => {
+    const hasTheme = localStorage.getItem('theme')
+    if (hasTheme) {
+      const localTheme = localStorage.getItem('theme') as Theme
+      localTheme === 'light' ? setTheme(localTheme) : setTheme('dark')
+    } else {
+      localStorage.setItem('theme', 'light')
+      setTheme('light')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    const hasLanguage = localStorage.getItem('language')
+    if (hasLanguage) {
+      const localLanguage = localStorage.getItem('language') as Language
+      localLanguage === 'ARABIC'
+        ? setLanguage(localLanguage)
+        : setLanguage('ENGLISH')
+    } else {
+      localStorage.setItem('language', 'ARABIC')
+      setLanguage('ARABIC')
+    }
+  }, [language])
 
   return (
     <div id='app' className={theme}>
