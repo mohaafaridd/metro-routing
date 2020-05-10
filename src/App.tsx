4b5ab864a0281d7ea73ddb046737b2314fcc2e2c
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-05-10 21:23:37 +0200

    added theme toggler button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { useContext } from 'react'
 import Search from './components/Search'
 import Path from './components/Path'
 import ThemeContext from './context/theme/themeContext'
+import ThemeToggler from './components/ThemeToggler'
 const App = () => {
   const { theme } = useContext(ThemeContext)
 
   return (
     <div id='app' className={theme}>
+      <ThemeToggler />
       <Search />
       <Path />
     </div>
---
+++ src/components/ThemeToggler.tsx
@@ -0,0 +1,44 @@
+import React, { useContext, useState, useEffect } from 'react'
+import Switch from 'react-switch'
+import { RiSunLine, RiMoonLine } from 'react-icons/ri'
+
+import { ThemeContext } from '../context'
+
+const ThemeToggler = () => {
+  const { theme, setTheme } = useContext(ThemeContext)
+  const [toggle, setToggle] = useState(theme === 'dark')
+
+  useEffect(() => {
+    setToggle(theme === 'dark')
+  }, [theme])
+
+  return (
+    <div className='flex justify-end p-2'>
+      <Switch
+        width={70}
+        handleDiameter={30}
+        boxShadow='0px 1px 5px rgba(0, 0, 0, 0.6)'
+        activeBoxShadow='0px 0px 1px 10px rgba(0, 0, 0, 0.2)'
+        onHandleColor='#fff'
+        onColor='#00adb5'
+        uncheckedIcon={
+          <div className='flex justify-center items-center h-full'>
+            <RiSunLine className='text-yellow-300' />
+          </div>
+        }
+        checkedIcon={
+          <div className='flex justify-center items-center h-full'>
+            <RiMoonLine className='text-white' />
+          </div>
+        }
+        checked={toggle}
+        onChange={(checked) => {
+          setTheme(checked ? 'dark' : 'light')
+          localStorage.setItem('theme', checked ? 'dark' : 'light')
+        }}
+      />
+    </div>
+  )
+}
+
+export default ThemeToggler
