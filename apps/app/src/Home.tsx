c09024288e1c9d153ae05810632232cfd3866c2d
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-12-15 16:43:37 +0200

    add all stations and translations

--- apps/app/src/Home.tsx
+++ apps/app/src/Home.tsx
@@ -9,7 +9,7 @@ export const Home = () => {
   const { t } = useLanguageContext();
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-gray-100 dark:bg-gray-900">
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-4 bg-gray-100 dark:bg-gray-900">
       <p className="rounded-none text-3xl font-bold underline">Hello World</p>
 
       <LanguageSwitch />
